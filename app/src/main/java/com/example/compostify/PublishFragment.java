69a10f5013f504911fce050c4095696e8d2d8c0e
Riddhiben Makwana <riddhimakwana77@gmail.com>
2024-03-21 14:21:25 -0400

    --Add clear button --upload Image only you click on post button

--- app/src/main/java/com/example/compostify/PublishFragment.java
+++ app/src/main/java/com/example/compostify/PublishFragment.java
@@ -12,7 +12,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -26,8 +25,6 @@ import com.example.compostify.adapters.PhotoAdapter;
 import com.example.compostify.databinding.FragmentPublishBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -45,16 +42,6 @@ public class PublishFragment extends Fragment {
     private String userId;
     private FragmentPublishBinding binding;
     private FirebaseFirestore db;
-    private AutoCompleteTextView edtTypeOfUser;
-    private AutoCompleteTextView edtTypeOfWaste;
-    private TextInputEditText edtWeight;
-    private TextInputEditText edtNaturalWeight;
-    private TextInputEditText edtMixWeight;
-    private TextInputEditText edtOtherDetails;
-
-    private TextInputLayout txtLayWeight;
-    private TextInputLayout txtLayNaturalWeight;
-    private TextInputLayout txtLayMixWeight;
     //
     private List<String> uploadedImageUrls = new ArrayList<>();
 
@@ -72,19 +59,28 @@ public class PublishFragment extends Fragment {
         initView();
 
         //Adding image button
-        Button btnUploadPhotos = binding.btnUploadPhotos;
-        btnUploadPhotos.setOnClickListener(new View.OnClickListener() {
+        binding.btnSelectPhotos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 pickImage();
             }
         });
 
-        Button btnPublish = binding.btnPost;
-        btnPublish.setOnClickListener(new View.OnClickListener() {
+        //Post Button
+        binding.btnPost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 publishData();
+            }
+        });
+
+        //reset the entire field
+        binding.btnClearPhotos.setOnClickListener(new  View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                clearFormFields();
+                binding.txtLayPhoto.setVisibility(View.GONE);
+                binding.btnSelectPhotos.setVisibility(View.GONE);
             }
         });
 
@@ -92,20 +88,12 @@ public class PublishFragment extends Fragment {
     }
 
     private void initView() {
-        edtTypeOfUser = binding.edtTypeOfUser;
-        edtTypeOfWaste = binding.edtTypeOfWaste;
-        edtWeight = binding.edtWeight;
-        edtNaturalWeight = binding.edtNaturalWeight;
-        edtMixWeight = binding.edtMixWeight;
-        edtOtherDetails = binding.edtOtherDetails;
-
-        txtLayWeight = binding.txtLayWeight;
-        txtLayNaturalWeight = binding.txtLayNaturalWeight;
-        txtLayMixWeight = binding.txtLayMixWeight;
 
         //Natural and mix weight will only show when type of user will select typeof waste "both"
-        txtLayNaturalWeight.setVisibility(View.GONE);
-        txtLayMixWeight.setVisibility(View.GONE);
+        binding.txtLayNaturalWeight.setVisibility(View.GONE);
+        binding.txtLayMixWeight.setVisibility(View.GONE);
+        binding.txtLayPhoto.setVisibility(View.GONE);
+        binding.btnSelectPhotos.setVisibility(View.GONE);
 
         //Fill data which were needed
 //        fillData();
@@ -115,17 +103,52 @@ public class PublishFragment extends Fragment {
                 android.R.layout.simple_dropdown_item_1line,
                 new String[]{"Natural Waste (5$ per 10Kg)", "Mix Waste (3$ per 10Kg)", "Both"}
         );
-        edtTypeOfWaste.setAdapter(wasteAdapter);
+        binding.edtTypeOfWaste.setAdapter(wasteAdapter);
 
         ArrayAdapter<String> userAdapter = new ArrayAdapter<>(
                 requireContext(),
                 android.R.layout.simple_dropdown_item_1line,
                 new String[]{"Buyer(Fertilizer Companies, etc.)", "Seller(Restaurants, cafe, Super Markets, etc.)"}
         );
-        edtTypeOfUser.setAdapter(userAdapter);
+        binding.edtTypeOfUser.setAdapter(userAdapter);
 
+        visibilityOfContent();
+
+        //changing background color of dropdown menus
+        AutoCompleteTextView autoCompleteTOWaste = binding.edtTypeOfWaste;
+        autoCompleteTOWaste.setDropDownBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(requireContext(), R.color.secondary)));
+        AutoCompleteTextView autoCompleteTOUser = binding.edtTypeOfUser;
+        autoCompleteTOUser.setDropDownBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(requireContext(), R.color.secondary)));
+    }
+
+    private void visibilityOfContent() {
         // Add TextWatcher to edtTypeOfWaste
-        edtTypeOfWaste.addTextChangedListener(new TextWatcher() {
+        binding.edtTypeOfUser.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int start, int before, int count) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // Check the selected type of user
+                String userType = editable.toString();
+                if ("Seller(Restaurants, cafe, Super Markets, etc.)".equals(userType)) {
+                    // Show the "Natural weight and Mix weight" field
+                    binding.txtLayPhoto.setVisibility(View.VISIBLE);
+                    binding.btnSelectPhotos.setVisibility(View.VISIBLE);
+                }else {
+                    binding.txtLayPhoto.setVisibility(View.GONE);
+                    binding.btnSelectPhotos.setVisibility(View.GONE);
+
+                }
+            }
+        });
+
+        binding.edtTypeOfWaste.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int start, int before, int count) {
             }
@@ -140,20 +163,14 @@ public class PublishFragment extends Fragment {
                 String wasteType = editable.toString();
                 if ("Both".equals(wasteType)) {
                     // Show the "Natural weight and Mix weight" field
-                    txtLayNaturalWeight.setVisibility(View.VISIBLE);
-                    txtLayMixWeight.setVisibility(View.VISIBLE);
-                }else {
-                    txtLayNaturalWeight.setVisibility(View.GONE);
-                    txtLayMixWeight.setVisibility(View.GONE);
+                    binding.txtLayNaturalWeight.setVisibility(View.VISIBLE);
+                    binding.txtLayMixWeight.setVisibility(View.VISIBLE);
+                } else {
+                    binding.txtLayNaturalWeight.setVisibility(View.GONE);
+                    binding.txtLayMixWeight.setVisibility(View.GONE);
                 }
             }
         });
-
-        //changing background color of dropdown menus
-        AutoCompleteTextView autoCompleteTOWaste = binding.edtTypeOfWaste;
-        autoCompleteTOWaste.setDropDownBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(requireContext(), R.color.secondary)));
-        AutoCompleteTextView autoCompleteTOUser = binding.edtTypeOfUser;
-        autoCompleteTOUser.setDropDownBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(requireContext(), R.color.secondary)));
     }
 
     private void pickImage() {
@@ -171,15 +188,14 @@ public class PublishFragment extends Fragment {
             if (clipData != null) {
                 for (int i = 0; i < clipData.getItemCount(); i++) {
                     Uri imageUri = clipData.getItemAt(i).getUri();
-                    uploadImage(imageUri);
+                    // Add the selected image URI to the list
+                    uploadedImageUrls.add(imageUri.toString());
                 }
             } else if (data.getData() != null) { // Handle single image selection
                 Uri imageUri = data.getData();
-                uploadImage(imageUri);
-            }else {
-                Uri imageUri = data.getData();
-                uploadImage(imageUri);
+                uploadedImageUrls.add(imageUri.toString());
             }
+            displayUploadedImages();
         }
     }
 
@@ -198,8 +214,8 @@ public class PublishFragment extends Fragment {
                             public void onSuccess(Uri uri) {
                                 String downloadUrl = uri.toString();
                                 uploadedImageUrls.add(downloadUrl);
-                                Toast.makeText(requireContext(), "Image uploaded successfully", Toast.LENGTH_SHORT).show();
-                                displayUploadedImages();
+//                                Toast.makeText(requireContext(), "Image uploaded successfully", Toast.LENGTH_SHORT).show();
+//                                displayUploadedImages();
                             }
                         });
                     }
@@ -221,40 +237,59 @@ public class PublishFragment extends Fragment {
 
     private void publishData() {
         if (validateInputs()) {
-            String typeOfUser = binding.edtTypeOfUser.getText().toString();
-            String typeOfWaste = binding.edtTypeOfWaste.getText().toString();
-            String totalWeight = binding.edtWeight.getText().toString();
-            String naturalWeight = binding.edtNaturalWeight.getText().toString();
-            String mixWeight = binding.edtMixWeight.getText().toString();
-            String otherDetails = binding.edtOtherDetails.getText().toString();
 
-            Map<String, Object> postDetails = new HashMap<>();
-            postDetails.put("typeOfUser", typeOfUser);
-            postDetails.put("typeOfWaste", typeOfWaste);
-            postDetails.put("totalWeight", totalWeight);
-            postDetails.put("naturalWasteWeight", naturalWeight);
-            postDetails.put("mixWasteWeight", mixWeight);
-            postDetails.put("otherDetails", otherDetails);
-            postDetails.put("imageUrls", uploadedImageUrls);
-            postDetails.put("postDateTime", FieldValue.serverTimestamp());
+            // Get logged-in user details
+//                    FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
-            db.collection("Publish")
-                    .add(postDetails)
-                    .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                        @Override
-                        public void onSuccess(DocumentReference documentReference) {
-                            Toast.makeText(requireContext(), "Post published successfully", Toast.LENGTH_SHORT).show();
-                            clearFormFields();
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Toast.makeText(requireContext(), "Failed to publish post", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-        }
-    }
+
+//                    if (currentUser != null) {
+//                        // Get user ID and email
+//                        String userId = currentUser.getUid();
+//                        String userEmail = currentUser.getEmail();
+
+            // Upload images only when the "Post" button is clicked
+            for (String imageUrl : uploadedImageUrls) {
+                uploadImage(Uri.parse(imageUrl));
+            }
+
+                        String typeOfUser = binding.edtTypeOfUser.getText().toString();
+                        String typeOfWaste = binding.edtTypeOfWaste.getText().toString();
+                        String totalWeight = binding.edtWeight.getText().toString();
+                        String naturalWeight = binding.edtNaturalWeight.getText().toString();
+                        String mixWeight = binding.edtMixWeight.getText().toString();
+                        String otherDetails = binding.edtOtherDetails.getText().toString();
+
+                        Map<String, Object> postDetails = new HashMap<>();
+//                        postDetails.put("userId", userId);
+//                        postDetails.put("userEmail", userEmail);
+                        postDetails.put("typeOfUser", typeOfUser);
+                        postDetails.put("typeOfWaste", typeOfWaste);
+                        postDetails.put("totalWeight", totalWeight);
+                        postDetails.put("naturalWasteWeight", naturalWeight);
+                        postDetails.put("mixWasteWeight", mixWeight);
+                        postDetails.put("otherDetails", otherDetails);
+                        postDetails.put("imageUrls", uploadedImageUrls);
+                        postDetails.put("postDateTime", FieldValue.serverTimestamp());
+
+                        db.collection("Publish")
+                                .add(postDetails)
+                                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                    @Override
+                                    public void onSuccess(DocumentReference documentReference) {
+                                        Toast.makeText(requireContext(), "Post published successfully", Toast.LENGTH_SHORT).show();
+                                        clearFormFields();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(requireContext(), "Failed to publish post: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+
+//                    }//get user
+        }//valid input
+}//publish data
 
 //    private void fillData() {
 //        userId = firebaseAuth.getCurrentUser().getUid();
@@ -276,72 +311,85 @@ public class PublishFragment extends Fragment {
         boolean isValid = true;
 
         // Validation for Type of User
-        if (edtTypeOfUser.getText().toString().isEmpty()) {
-            edtTypeOfUser.setError("Type of User is required");
+        if (binding.edtTypeOfUser.getText().toString().isEmpty()) {
+            binding.edtTypeOfUser.setError("Type of User is required");
             isValid = false;
         }
 
         // Validation for Type of Waste
-        if (edtTypeOfWaste.getText().toString().isEmpty()) {
-            edtTypeOfWaste.setError("Type of Waste is required");
+        if (binding.edtTypeOfWaste.getText().toString().isEmpty()) {
+            binding.edtTypeOfWaste.setError("Type of Waste is required");
             isValid = false;
         }
 
         // Validation for Weight
-        String weight = edtWeight.getText().toString();
+        String weight = binding.edtWeight.getText().toString();
         if (weight.isEmpty()) {
-            txtLayWeight.setError("Weight is required");
+            binding.txtLayWeight.setError("Weight is required");
             isValid = false;
         } else {
             int totalWeight = Integer.parseInt(weight);
             if (totalWeight < 10) { // Minimum total weight requirement
-                txtLayWeight.setError("Minimum total weight should be 10 kg");
+                binding.txtLayWeight.setError("Minimum total weight should be 10 kg");
                 isValid = false;
             } else {
-                txtLayWeight.setError(null);
+                binding.txtLayWeight.setError(null);
             }
         }
 
         // Validation for Price (only if the user is a seller)
-        String userType = edtTypeOfUser.getText().toString();
-        if ("Seller(Restaurants, cafe, Super Markets, etc.)".equals(userType)) {
-            String naturalWeight = edtNaturalWeight.getText().toString();
-            String mixWeight = edtMixWeight.getText().toString();
+        String userType = binding.edtTypeOfWaste.getText().toString();
+        if ("Both".equals(userType)) {
+            String naturalWeight = binding.edtNaturalWeight.getText().toString();
+            String mixWeight = binding.edtMixWeight.getText().toString();
+            boolean isNaturalWeightValid = true;
+            boolean isMixWeightValid = true;
+
             if (naturalWeight.isEmpty()) {
-                txtLayNaturalWeight.setError("Price is required for sellers");
-                isValid = false;
+                binding.txtLayNaturalWeight.setError("Natural Waste Weight is required.");
+                isNaturalWeightValid = false;
             } else {
                 int naturalWeightValue = Integer.parseInt(naturalWeight);
                 if (naturalWeightValue < 5) { // Minimum natural waste weight requirement
-                    txtLayNaturalWeight.setError("Minimum natural waste weight should be 5 kg");
-                    isValid = false;
+                    binding.txtLayNaturalWeight.setError("Minimum natural waste weight should be 5 kg");
+                    isNaturalWeightValid = false;
                 } else {
-                    txtLayNaturalWeight.setError(null);
+                    binding.txtLayNaturalWeight.setError(null);
                 }
             }
+
             if (mixWeight.isEmpty()) {
-                txtLayMixWeight.setError("Price is required for sellers");
-                isValid = false;
+                binding.txtLayMixWeight.setError("Mix Waste Weight is required.");
+                isMixWeightValid = false;
             } else {
                 int mixWeightValue = Integer.parseInt(mixWeight);
                 if (mixWeightValue < 5) { // Minimum mix waste weight requirement
-                    txtLayMixWeight.setError("Minimum mix waste weight should be 5 kg");
-                    isValid = false;
+                    binding.txtLayMixWeight.setError("Minimum mix waste weight should be 5 kg");
+                    isMixWeightValid = false;
                 } else {
-                    txtLayMixWeight.setError(null);
+                    binding.txtLayMixWeight.setError(null);
                 }
             }
+
+            // Check if both natural weight and mix weight are valid
+            isValid = isNaturalWeightValid && isMixWeightValid;
         }
+
         return isValid;
     }
     private void clearFormFields() {
-        edtTypeOfUser.setText("");
-        edtTypeOfWaste.setText("");
-        edtWeight.setText("");
-        edtNaturalWeight.setText("");
-        edtMixWeight.setText("");
-        edtOtherDetails.setText("");
+        binding.edtTypeOfUser.setText("");
+        binding.edtTypeOfWaste.setText("");
+        binding.edtWeight.setText("");
+        binding.edtNaturalWeight.setText("");
+        binding.edtMixWeight.setText("");
+        binding.edtOtherDetails.setText("");
+
+        // Clear uploaded image URLs and notify adapter
         uploadedImageUrls.clear();
-        binding.rvWastePhotos.getAdapter().notifyDataSetChanged();
+        RecyclerView.Adapter adapter = binding.rvWastePhotos.getAdapter();
+        if (adapter != null) {
+            adapter.notifyDataSetChanged();
+        }
     }
 }
